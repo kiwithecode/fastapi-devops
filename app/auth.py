27ec33d0e0d39f35a1f088bc7925e5bdd8f07b90
kiwithecode <wowcelodion@gmail.com>
2025-07-10 00:38:07 -0500

    accionts: cambios en el main.yml

--- app/auth.py
+++ app/auth.py
@@ -1,17 +1,14 @@
 from jose import jwt
-from datetime import datetime, timedelta, UTC
-
+from datetime import datetime, timedelta, timezone
 
 SECRET_KEY = "uNs3Cr3t0-larg0-y-coMpL3j0"
-
 
 def verify_apikey(header_key: str, expected_key: str) -> bool:
     return header_key == expected_key
 
-
 def generate_jwt(data: dict) -> str:
     payload = data.copy()
     payload.update({
-        "exp": datetime.now(UTC) + timedelta(seconds=60)
+        "exp": datetime.now(timezone.utc) + timedelta(seconds=60)
     })
     return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
