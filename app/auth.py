a1c5b792a4e0d6496c1ed4c9c76fcc87065847ee
kiwithecode <wowcelodion@gmail.com>
2025-07-10 00:39:39 -0500

    slint

--- app/auth.py
+++ app/auth.py
@@ -1,10 +1,13 @@
 from jose import jwt
 from datetime import datetime, timedelta, timezone
 
+
 SECRET_KEY = "uNs3Cr3t0-larg0-y-coMpL3j0"
+
 
 def verify_apikey(header_key: str, expected_key: str) -> bool:
     return header_key == expected_key
+
 
 def generate_jwt(data: dict) -> str:
     payload = data.copy()
